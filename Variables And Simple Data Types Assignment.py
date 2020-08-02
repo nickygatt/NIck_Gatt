3e7e07fae362288b36cff834a0ec7ce2295e6ae9
nickygatt <68790753+nickygatt@users.noreply.github.com>
2020-08-02 19:48:23 -0400

    Add files via upload

---
+++ Variables And Simple Data Types Assignment.py
@@ -0,0 +1,79 @@
+1
+string_var = "nick"
+print(string_var.lower())
+
+2
+name_var = " gatt"
+print(name_var.upper())
+
+3
+greeting_var = "Hello, "
+
+string_var = "nick"
+
+name_var = " gatt"
+
+output_variable = greeting_var + str(string_var.upper()) + name_var.lower()
+
+print(output_variable)
+
+4
+greeting_var = "Hello, "
+
+string_var = "nick"
+
+name_var = " gatt"
+
+output_variable = greeting_var + "\n" + str(string_var.upper()) + "\n" + name_var.lower()
+
+print(output_variable)
+
+5
+string_var = "\"Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible - Francis of Assisi\""
+
+print(string_var)
+
+6
+Four_var = .4
+Eight_var = .8
+
+math_result = Four_var + Eight_var
+
+print(math_result)
+
+7
+numberFour = .4
+numberEight = .8
+
+math_result = numberFour + numberEight
+
+print(math_result)
+
+math_result = numberFour - numberEight
+
+print(math_result)
+
+math_result = numberFour * numberEight
+
+print(math_result)
+
+math_result = numberFour / numberEight
+
+print(math_result)
+
+9
+string_variable = "Today is July "
+
+number_variable = 22
+
+
+10
+string_variable = "Today is "
+
+day_variable = 22
+
+month_variable = " of the month of July"
+
+result_variable = string_variable + str(day_variable) + "\n\t\t" + month_variable
+
+print(result_variable)
